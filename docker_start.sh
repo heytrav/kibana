fe5b5a0ef62ef2cfd47c40741899d724080348ff
Travis Holton <travis@ideegeo.com>
2015-04-14 19:46:35 +1200

    change localhost to environment variable

--- docker_start.sh
+++ docker_start.sh
@@ -2,6 +2,8 @@
 
 SSH_PASSWORD=`perl -MJSON::PP -e 'print decode_json(qx{curl --cacert /etc/ssl/certs/coreos/ca.pem --cert /etc/ssl/certs/coreos/server.crt --key /etc/ssl/certs/coreos/server.key -L $ENV{ETCD_SERVICE}/v2/keys/iwmn/kibana/ssh_password})->{node}->{value};'`
 
+sed -i 's/localhost/'"$ELASTICSEARCH_LB_SERVICE_HOST"'/g' /opt/kibana/config/kibana.yml
+
 if [ -n "$SSH_PASSWORD" ]; then
     echo "Configuring ssh: setting root password to ${SSH_PASSWORD}"
     echo "root:$SSH_PASSWORD" | chpasswd
